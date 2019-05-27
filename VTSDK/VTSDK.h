c753e26f356e7107811b98663fecd6249e3bbb69
Thanh Tran Van <ttv>
2019-05-27 21:18:17 +0700

    Initial Commit

---
+++ VTSDK/VTSDK.h
@@ -0,0 +1,19 @@
+//
+//  VTSDK.h
+//  VTSDK
+//
+//  Created by Thanh Tran Van on 5/27/19.
+//  Copyright © 2019 Thanh Tran Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTSDK.
+FOUNDATION_EXPORT double VTSDKVersionNumber;
+
+//! Project version string for VTSDK.
+FOUNDATION_EXPORT const unsigned char VTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTSDK/PublicHeader.h>
+
+
